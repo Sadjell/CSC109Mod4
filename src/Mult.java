5d5c92a8b4eccc92a817dc49b278942fb4b42398
Sadjell <57645947+Sadjell@users.noreply.github.com>
2021-02-22 20:30:29 -0500

    Print statement to ask other input

--- src/Mult.java
+++ src/Mult.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
 public class Mult {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Please enter a number");
+		Scanner scanner = new Scanner(System.in);
+		int userInput = scanner.nextInt();
+		System.out.println("Enter another number.");
 
 	}
 
