d890790d3f9d6c95b42ec00a9b785849842986b8
Sadjell <57645947+Sadjell@users.noreply.github.com>
2021-02-22 21:20:53 -0500

    Print line with result

--- src/Mult.java
+++ src/Mult.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class Mult {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Please enter a number");
+		Scanner scanner = new Scanner(System.in);
+		int userInput = scanner.nextInt();
+
+		System.out.println("Enter another number.");
+		int userInput2 = scanner.nextInt();
+		
+		System.out.println("The product of " + userInput + " and " + userInput2 + " is " + userInput*userInput2 + ".");
+
 
 	}
 
