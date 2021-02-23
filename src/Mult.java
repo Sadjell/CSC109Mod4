6d028e11a75b53eb783f050964b7d86aa3a1db50
Sadjell <57645947+Sadjell@users.noreply.github.com>
2021-02-22 19:16:29 -0500

    declaration statement for the scanner that will be used to take the user's input

--- src/Mult.java
+++ src/Mult.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 public class Mult {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Please enter a number");
+		Scanner scanner = new Scanner(System.in);
 
 	}
 
